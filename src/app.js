6686e129cb65fb27ffb4517c3b20f19a2c3c1e56
Pa-ola28 <paolasuarezceron1@gmail.com>
2025-06-26 12:42:31 +0000

    cambiocolor

--- src/app.js
+++ src/app.js
@@ -6,10 +6,11 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  //write your code here
+    //write your code here
 
-   const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     const palos = ["♠", "♥", "♦", "♣"];
+
 
     function generarCartaAleatoria() {
         const valor = valores[Math.floor(Math.random() * valores.length)];
@@ -18,16 +19,26 @@ window.onload = function () {
     }
 
     const { valor, palo } = generarCartaAleatoria();
-    document.querySelector(".valor").innerHTML = valor;
-    document.querySelector(".palo-superior").innerHTML = palo;
-    document.querySelector(".palo-inferior").innerHTML = palo;
 
-    let nuevoTexto ="Bienvenido esta es tu Carta Aleatoria!";
-    let titulo =document.createElement("h1");
+    const color = palo === "♥" || palo === "♦" ? "red" : "black";
+
+    document.querySelector(".valor").innerHTML = valor;
+    document.querySelector(".valor").style.color = color;
+
+    document.querySelector(".palo-superior").innerHTML = palo;
+    document.querySelector(".palo-superior").style.color = color;
+
+    document.querySelector(".palo-inferior").innerHTML = palo;
+    document.querySelector(".palo-inferior").style.color = color;
+
+
+
+    let nuevoTexto = "Bienvenido esta es tu Carta Aleatoria!";
+    let titulo = document.createElement("h1");
     titulo.textContent = nuevoTexto;
 
     document.body.prepend(titulo);
 
-    
+
 };
 
