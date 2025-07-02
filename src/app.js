ec2b8e922c7401534a7e9dd613fabb2ffcff6f74
Pa-ola28 <paolasuarezceron1@gmail.com>
2025-07-02 10:21:35 +0000

    agregar-boton

--- src/app.js
+++ src/app.js
@@ -6,32 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
+    nuevaCarta();
+    document.querySelector("#nuevaCartaBtn").addEventListener("click", nuevaCarta)
+}
     //write your code here
 
-    const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    const palos = ["♠", "♥", "♦", "♣"];
+  function nuevaCarta () {
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const palos = ["♠", "♥", "♦", "♣"];
 
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+  const palo = palos[Math.floor(Math.random() * palos.length)];
+  const color = palo === "♥" || palo === "♦" ? "red" : "black";
 
-    function generarCartaAleatoria() {
-        const valor = valores[Math.floor(Math.random() * valores.length)];
-        const palo = palos[Math.floor(Math.random() * palos.length)];
-        return { valor, palo };
-    }
+  document.querySelector(".valor").innerHTML = valor;
+  document.querySelector(".valor").style.color = color;
 
-    const { valor, palo } = generarCartaAleatoria();
+  document.querySelector(".palo-superior").innerHTML = palo;
+  document.querySelector(".palo-superior").style.color = color;
 
-    const color = palo === "♥" || palo === "♦" ? "red" : "black";
-
-    document.querySelector(".valor").innerHTML = valor;
-    document.querySelector(".valor").style.color = color;
-
-    document.querySelector(".palo-superior").innerHTML = palo;
-    document.querySelector(".palo-superior").style.color = color;
-
-    document.querySelector(".palo-inferior").innerHTML = palo;
-    document.querySelector(".palo-inferior").style.color = color;
-
-
+  document.querySelector(".palo-inferior").innerHTML = palo;
+  document.querySelector(".palo-inferior").style.color = color;
+}
 
     let nuevoTexto = "Bienvenido esta es tu Carta Aleatoria!";
     let titulo = document.createElement("h1");
@@ -40,5 +36,5 @@ window.onload = function () {
     document.body.prepend(titulo);
 
 
-};
+
 
